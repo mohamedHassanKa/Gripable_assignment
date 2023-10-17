802f120aa3be6fe891c4faab43618cb6f7a080bd
Abdulazeez Oriaje <jaycole154@gmail.com>
2023-10-17 22:58:05 +0100

    cleaning up

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'main_app.dart';
 import 'utils/state_logger.dart';
 
 Future main() async {
-  await dotenv.load(fileName: "environments/.env");
-
   runApp(const ProviderScope(
       observers: kDebugMode ? [StateLogger()] : null, child: MyApp()));
 }
